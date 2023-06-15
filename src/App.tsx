d3943b9d90d509231794d274bb98174462d46ed2
Niteesh-chowdary <niteesh.chowdary25@gmail.com>
2023-06-15 15:03:08 -0400

    customhook:data-fetch and controller:abort-cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     }}>
       <GridItem area='nav'><NavBar></NavBar></GridItem>
       <Show above='lg'><GridItem area='aside'>Aside</GridItem></Show>
-      <GridItem area='main'><GameGrid/></GridItem>
+      <GridItem area='main'><GameGrid></GameGrid></GridItem>
     </Grid>
   )
 }
