8b5c19d1e44367149ab4f12d177feeb0baade074
Niteesh-chowdary <niteesh.chowdary25@gmail.com>
2023-06-23 17:31:36 -0400

    Refactoring-fetch games with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,9 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface GameGridProps{
   gameQuery:GameQuery
@@ -15,7 +14,7 @@ function GameGrid({gameQuery}:GameGridProps) {
   const skeletons = [1, 2, 3, 4, 5, 6,7,8,9,10];
   return (
     <>
-      {error && <Text>{error}</Text>}
+      {error && <Text>{error.message}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
@@ -25,7 +24,7 @@ function GameGrid({gameQuery}:GameGridProps) {
           skeletons.map(Skeleton => (
             <GameCardContainer key={Skeleton}><GameCardSkeleton></GameCardSkeleton></GameCardContainer>
           ))}
-        {data.map((game) => (
+        {data?.results.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
