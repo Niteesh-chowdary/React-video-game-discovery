dfda556f9c309b1ebc8bf4eac39b2025dcc6fe4f
niteesh-chowdary <niteesh.chowdary25@gmail.com>
2023-07-01 00:38:07 -0400

    Refactor-move entity interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,41 +1,50 @@
-import { Button, HStack, Heading, Image, List, ListItem } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import { Genre } from "../entities/Genre";
 import GetCroppedImages from "../services/GetCroppedImages";
 import useGameQueryStore from "../store";
 
-
 function GenreList() {
   const { data } = useGenres();
-  const selectedGenreId = useGameQueryStore(s=>s.gameQuery.genreId);
-  const setGenreId = useGameQueryStore(s=>s.setGenreId);
-  return(
-  <>
-    <Heading fontSize='2xl' marginBottom={5}>Genres</Heading>
-    <List>
-      {data?.results.map((genre) => (
-        <ListItem paddingY={"5px"} key={genre.id}>
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={GetCroppedImages(genre.image_background)}
-              objectFit="cover"
-            />
-            <Button
-              whiteSpace={"normal"}
-              textAlign={"left"}
-              fontWeight={genre.id === selectedGenreId? "bold" : "normal"}
-              fontSize="lg"
-              variant="link"
-              onClick={() => setGenreId(genre.id)}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
-  </>
+  const selectedGenreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const setGenreId = useGameQueryStore((s) => s.setGenreId);
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={5}>
+        Genres
+      </Heading>
+      <List>
+        {data?.results.map((genre) => (
+          <ListItem paddingY={"5px"} key={genre.id}>
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                src={GetCroppedImages(genre.image_background)}
+                objectFit="cover"
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
+                fontSize="lg"
+                variant="link"
+                onClick={() => setGenreId(genre.id)}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,6 +1,6 @@
 import { HStack, Icon } from "@chakra-ui/react";
-import { IconType } from 'react-icons';
-import { BsGlobe } from 'react-icons/bs';
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
 import {
   FaAndroid,
   FaApple,
@@ -9,31 +9,35 @@ import {
   FaWindows,
   FaXbox,
 } from "react-icons/fa";
-import { MdPhoneIphone } from 'react-icons/md';
-import { SiNintendo } from 'react-icons/si';
-import { Platform } from "../hooks/usePlatforms";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { Platform } from "../entities/Platform";
 
 interface PlatformIconListProps {
   platforms: Platform[];
 }
 
-const iconMap:{[key:string]:IconType} = {
-    pc:FaWindows,
-    playstation:FaPlaystation,
-    xbox:FaXbox,
-    nintendo: SiNintendo,
-    mac:FaApple,
-    linux:FaLinux,
-    ios:MdPhoneIphone,
-    web:BsGlobe,
-    android:FaAndroid
-}
+const iconMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  web: BsGlobe,
+  android: FaAndroid,
+};
 
 function PlatformIconList({ platforms }: PlatformIconListProps) {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color='gray.500' key={platform.name}/>
+        <Icon
+          as={iconMap[platform.slug]}
+          color="gray.500"
+          key={platform.name}
+        />
       ))}
     </HStack>
   );
