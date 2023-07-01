5039b99fb8297f86cdceecc15b80fd2996eac097
niteesh-chowdary <niteesh.chowdary25@gmail.com>
2023-07-01 00:33:54 -0400

    Fetch Game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,26 +1,31 @@
-import { Game } from '../hooks/useGames';
-import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
-import PlatformIconList from './PlatformIconList';
-import CriticScore from './CriticScore';
-import GetCroppedImages from '../services/GetCroppedImages';
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import GetCroppedImages from "../services/GetCroppedImages";
+import { Link } from "react-router-dom";
 
-interface GameCardProps{
-    game:Game
+interface GameCardProps {
+  game: Game;
 }
 
-function GameCard({game}:GameCardProps){
-    return (
-        <Card width='250px' borderRadius={10} overflow='hidden'>
-            <Image src={GetCroppedImages(game.background_image)}/>
-            <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
-                <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
-                <CriticScore metacritic={game.metacritic}></CriticScore>
-                </HStack>
-            </CardBody>
-        </Card>
-    )
+function GameCard({ game }: GameCardProps) {
+  return (
+    <Card width="250px" borderRadius={10} overflow="hidden">
+      <Image src={GetCroppedImages(game.background_image)} />
+      <CardBody>
+        <Link to={"/games/" + game.slug}>
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </Link>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore metacritic={game.metacritic}></CriticScore>
+        </HStack>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default GameCard;
