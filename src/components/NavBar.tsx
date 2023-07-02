3213abf2276314e66e644e0e36cfa3219913e7ca
niteesh-chowdary <niteesh.chowdary25@gmail.com>
2023-07-02 14:53:47 -0400

    Fixing Navbar for search in different pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,19 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from '../assets/logo.webp'
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
-
-function NavBar(){
-
-    return <HStack padding='10px'>
-        <Image src={logo} boxSize='60px'></Image>
-        <SearchInput></SearchInput>
-        <ColorModeSwitch></ColorModeSwitch>
+function NavBar() {
+  return (
+    <HStack padding="10px">
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover"></Image>
+      </Link>
+      <SearchInput></SearchInput>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
+  );
 }
 
 export default NavBar;
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,18 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 function SearchInput() {
   const ref = useRef<HTMLInputElement>(null);
-  const {setSearchText} = useGameQueryStore();
+  const { setSearchText } = useGameQueryStore();
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
